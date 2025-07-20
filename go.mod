fca1cc41f7abc8e38ac4403db80589e1044f571b
Robert Gleason <l2ob122@gmail.com>
2025-07-19 23:41:13 -0700

    added modd for hot reloading

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Epiq122/go-lens
+
+go 1.24.0
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprint(w, "<h1> Welcome to my nightmare </h1>")
 }
 
