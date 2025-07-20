ee5fe45d6dad8a915e5ff0f2bc74b980298849bd
Robert Gleason <l2ob122@gmail.com>
2025-07-20 12:24:27 -0700

    created a faq page to end of section

--- main.go
+++ main.go
@@ -16,7 +16,27 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// custom routing
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>
+  <li>
+    <b>Is there a free version?</b>
+    Yes! We offer a free trial for 30 days on any paid plans.
+  </li>
+  <li>
+    <b>What are your support hours?</b>
+    We have support staff answering emails 24/7, though response
+    times may be a bit slower on weekends.
+  </li>
+  <li>
+    <b>How do I contact support?</b>
+    Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+  </li>
+</ul>
+`)
+
+}
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
@@ -24,20 +44,9 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
+	case "/faq":
+		faqHandler(w, r)
 
-}
-
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
@@ -46,6 +55,7 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/faq", faqHandler)
 	fmt.Println("starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
