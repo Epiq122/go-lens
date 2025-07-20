da16873ee5fad9aa0d32d4f02fbb5a335de92ad5
Robert Gleason <l2ob122@gmail.com>
2025-07-20 16:23:15 -0700

    created the home template and added the routing and error information into the homeHandler

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprint(w, "<h1> Welcome to my nightmare </h1>")
+	w.Header().Set("Content-Type", "text/html")
+	tpl, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There wads an error parsing the template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -48,5 +61,8 @@ func main() {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 	fmt.Println("starting the server on :3000...")
-	http.ListenAndServe(":3000", r)
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		fmt.Println("Server error:", err)
+	}
 }
