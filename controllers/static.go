838064a90c3374b555f8c1d4b496584857be7d29
Robert Gleason <l2ob122@gmail.com>
2025-07-20 17:38:38 -0700

    refactor template handling by introducing Static controller and simplifying route setup, changed code inside the main to refelect changes in controllers and static

---
+++ controllers/static.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"github.com/Epiq122/go-lens/views"
+	"net/http"
+)
+
+type Static struct {
+	Template views.Template
+}
+
+func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	static.Template.Execute(w, nil)
+}
+func StaticHandler(tpl views.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
--- main.go
+++ main.go
@@ -2,48 +2,38 @@ package main
 
 import (
 	"fmt"
+	"github.com/Epiq122/go-lens/controllers"
+	"github.com/Epiq122/go-lens/views"
 	_ "github.com/Epiq122/go-lens/views"
 	"github.com/go-chi/chi/v5"
-	"html/template"
-	"log"
 	"net/http"
+	"path/filepath"
 )
-
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	tpl, err := template.ParseFiles(filepath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There wads an error parsing the template", http.StatusInternalServerError)
-		return
-	}
-	tpl.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/home.gohtml")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/contact.gohtml")
-
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/faq.gohtml")
-
-}
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 	fmt.Println("starting the server on :3000...")
-	err := http.ListenAndServe(":3000", r)
-	if err != nil {
-		fmt.Println("Server error:", err)
-	}
+	http.ListenAndServe(":3000", r)
 }
