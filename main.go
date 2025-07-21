b4eeb18d37010f5fd7934bea3fca73c4fc292427
Robert Gleason <l2ob122@gmail.com>
2025-07-20 18:09:45 -0700

    add embedded file system support for template parsing

--- main.go
+++ main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/Epiq122/go-lens/controllers"
+	"github.com/Epiq122/go-lens/templates"
 	"github.com/Epiq122/go-lens/views"
 	_ "github.com/Epiq122/go-lens/views"
 	"github.com/go-chi/chi/v5"
 	"net/http"
-	"path/filepath"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
-	r.Get("/", controllers.StaticHandler(tpl))
+	r.Get("/", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
-	r.Get("/contact", controllers.StaticHandler(tpl))
+	r.Get("/contact", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
-	r.Get("/faq", controllers.StaticHandler(tpl))
+	r.Get("/faq", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
